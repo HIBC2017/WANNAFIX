32928e747089329f11802c5e6b00919fecc4b126
HIBC <contact@haveibeencompromised.com>
2017-05-15 20:15:01 +0000

    NEW SINKHOLE
    Just added another sinkhole, will recompile the binary soon.

--- WANNAFIX.cpp
+++ WANNAFIX.cpp
@@ -33,7 +33,8 @@ int main()
 
         char domainone[500]="\n216.58.197.132 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
         char domaintwo[500]"\n54.153.0.145 ifferfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
-        char domainthree[500]="\n216.58.197.132 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";;
+        char domainthree[500]"\n207.154.243.152 ayylmaotjhsstasdfasdfasdfasdfasdfasdfasdf.com";
+        char domainfour[500]="\n216.58.197.132 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";;
         FILE *fptr;
 
         fptr = fopen("C:\\Windows\\System32\\drivers\\etc\\hosts", "a");
@@ -41,6 +42,7 @@ int main()
         fprintf(fptr,"%s", domainone);
         fprintf(fptr,"%s", domaintwo);
         fprintf(fptr,"%s", domainthree);
+        fprintf(fptr,"%s", domainfour);
         fclose(fptr);
     }
     
