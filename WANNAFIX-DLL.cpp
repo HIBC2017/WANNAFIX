7b29f9297579dad8108f2b442ff7b354d84e46f8
Mark C <mc@blaidd.cymru>
2017-05-15 16:55:31 +0100

    Added a DLL version that can be run over a network - just get the machine to run "rundll32 \\path\to\WANNAFIX.dll,0" (the 'comma-0' is padding, can be any number).

---
+++ WANNAFIX-DLL.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+// I use mingw... sorry...
+// #include <Windows.h>
+// #include <Winuser.h>
+// build instructions under linux:
+// $> i686-w64-mingw32-g++ -c -DBUILDING_EXAMPLE_DLL WANNAFIX-DLL.cpp 
+// $> i686-w64-mingw32-g++ -shared -o WANNAFIX.dll WANNAFIX-DLL.o -Wl,--out-implib,WANNAFIX-DLL.a
+// dependencies: sudo dnf install mingw32-winpthreads* mingw32-gcc-c++.x86_64 
+#include <windows.h>
+#include <winuser.h>
+
+int addDomainsToHost()
+{
+    // FIXME add conditional to check that these are already in place... 
+    char domainone[500]="\n216.58.197.132 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
+    char domaintwo[500]="\n54.153.0.145 ifferfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
+    char domainthree[500]="\n216.58.197.132 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";;
+    FILE *fptr;
+    fptr = fopen("C:\\Windows\\System32\\drivers\\etc\\hosts", "a");
+    fprintf(fptr,"%s", domainone);
+    fprintf(fptr,"%s", domaintwo);
+    fprintf(fptr,"%s", domainthree);
+    fclose(fptr);
+}
+
+int disableSMBv1()
+{
+    // FIXME add conditional to *not* do this if SMBv1 is disabled
+    system("powershell.exe Set-ItemProperty -Path \"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanServer\\Parameters\" SMB1 -Type DWORD -Value 0 -Force");
+    // commented this out so as to not continually restart machines... 
+    //system("shutdown -r -t 15");
+}
+
+int createMutex()
+{
+    HANDLE mutexObj  = CreateMutex(NULL,TRUE,"Global\\MsWinZonesCacheCounterMutexA0");
+    if(GetLastError() == ERROR_ALREADY_EXISTS){
+        //printf("\n FATAL: There was an error creating the MUTEX, your machine is already infected or you have run this script previously! Error code: %d", GetLastError());
+        while(1){
+            Sleep(10000);
+        }
+    }
+    HWND hWnd = GetConsoleWindow(); 
+    ShowWindow(hWnd, SW_HIDE);
+    while(1){
+        Sleep(10000);
+    }
+    // FIXME this will probably never run, owing to the mutex being in place...
+    return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason, LPVOID lpvReserved)
+{
+    // what does this do?
+    char first;
+    // two can play at this game...
+    char moar_magic;
+    addDomainsToHost();
+    disableSMBv1();
+    createMutex();
+    return 0;
+}
